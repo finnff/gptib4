cb613a00a80604835a2fa24fe1979c5b907f13a5
HarrySpierenburg <harry.spierenburg@student.hu.nl>
2019-03-27 13:41:48 +0100

    update

--- GPTIVB4/RPIBluetooth/mainNotBlocking.cpp
+++ GPTIVB4/RPIBluetooth/mainNotBlocking.cpp
@@ -94,12 +94,3 @@ int main()
         clientsock->close();
     }
 }
-
-void exit_signal_handler(int signo)
-{
-    if (signo == SIGINT)
-    {
-        BP.reset_all(); // Reset everything so there are no run-away motors
-        exit(-2);
-    }
-}
