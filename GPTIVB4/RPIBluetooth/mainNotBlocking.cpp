d4424c34133fe6f35fb618946d8984bf237414cc
HarrySpierenburg <harry.spierenburg@student.hu.nl>
2019-03-27 12:37:12 +0100

    update

--- GPTIVB4/RPIBluetooth/mainNotBlocking.cpp
+++ GPTIVB4/RPIBluetooth/mainNotBlocking.cpp
@@ -1,66 +1,89 @@
 #include "BluetoothSocket.h"
 #include <unistd.h>
 #include "BrickPi3.h" // for BrickPi3
-#include <iostream>   
+#include <iostream>
 #include <signal.h>
 using namespace std;
 
 BrickPi3 BP;
 
-
-
-
-int main() {
+int main()
+{
     BP.offset_motor_encoder(PORT_A, BP.get_motor_encoder(PORT_A));
     BP.offset_motor_encoder(PORT_B, BP.get_motor_encoder(PORT_B));
 
-	BluetoothServerSocket serversock(2, 1);  //2 is het channel-number
-	cout << "listening" << endl;
-	while(true) {
-		BluetoothSocket* clientsock = serversock.accept();
-		cout << "accepted from " << clientsock->getForeignAddress().getAddress() << endl;
-		MessageBox& mb = clientsock->getMessageBox();
-			
-		string input;
-		while(mb.isRunning()) {
-			input = mb.readMessage();  //blokkeert niet
-			if(input != "") {
-                cout << endl << input << endl;
+    BluetoothServerSocket serversock(2, 1); //2 is het channel-number
+    cout << "listening" << endl;
+    while (true)
+    {
+        BluetoothSocket *clientsock = serversock.accept();
+        cout << "accepted from " << clientsock->getForeignAddress().getAddress() << endl;
+        MessageBox &mb = clientsock->getMessageBox();
+
+        string input;
+        while (mb.isRunning())
+        {
+            input = mb.readMessage(); //blokkeert niet
+            if (input != "")
+            {
+                cout << endl
+                     << input << endl;
             }
-            if (input = RIGHT) {
+            if (input = "RIGHT")
+            {
                 BP.set_motor_power(PORT_A, -20);
-                BP.set_motor_power(PORT_B, 20);    
+                BP.set_motor_power(PORT_B, 20);
             }
-            else if(input = LEFT)
+            else if (input = "LEFT")
             {
                 BP.set_motor_power(PORT_A, 20);
-                BP.set_motor_power(PORT_B, -20);  
+                BP.set_motor_power(PORT_B, -20);
             }
-            else if(input = UP)
+            else if (input = "UP")
             {
                 BP.set_motor_power(PORT_A, -20);
                 BP.set_motor_power(PORT_B, -20);
             }
-            else if(input = DOWN)
+            else if (input = "DOWN")
             {
                 BP.set_motor_power(PORT_A, 20);
                 BP.set_motor_power(PORT_B, 20);
             }
-            
-			//doe andere dingen.
-			cout << ".";
-			cout.flush();
-			usleep(500000); // wacht 500 ms
-		}	
-		
-		clientsock->close();
-			
-	}
+            else if (input = "MIDDLE")
+            {
+                BP.reset_all();
+            }
+            else if (input = "A")
+            {
+                BP.set_motor_power(PORT_A, -100);
+                BP.set_motor_power(PORT_B, -100);
+            }
+            else if (input = "B")
+            {
+                BP.set_motor_power(PORT_A, 100);
+                BP.set_motor_power(PORT_B, 100);
+            }
+            else if (input = "C")
+            {
+                BP.set_motor_power(PORT_A, -100);
+                BP.set_motor_power(PORT_B, 100);
+            }
+
+            //doe andere dingen.
+            cout << ".";
+            cout.flush();
+            usleep(500000); // wacht 500 ms
+        }
+
+        clientsock->close();
+    }
 }
 
-void exit_signal_handler(int signo){
-	if(signo == SIGINT){
-		BP.reset_all();    // Reset everything so there are no run-away motors
-    		exit(-2);
-  }
+void exit_signal_handler(int signo)
+{
+    if (signo == SIGINT)
+    {
+        BP.reset_all(); // Reset everything so there are no run-away motors
+        exit(-2);
+    }
 }
