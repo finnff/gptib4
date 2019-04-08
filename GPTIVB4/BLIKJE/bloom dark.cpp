e5f09efdd94e84d84bdb877c59c27c06128f9a30
finnf <finnf@protonmail.com>
2019-04-08 14:01:36 +0200

    ajsjd

--- GPTIVB4/BLIKJE/bloom dark.cpp
+++ GPTIVB4/BLIKJE/bloom dark.cpp
@@ -139,7 +139,7 @@ void kleurscanA(){
             BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_OFF); // turns off rbg
             BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_COLOR_OFF);
             for(int i=0; i<2;i++){
-                rgbaf(rgbA, false);
+                cout << "Sensor A"<< rgbaf(rgbA, false);
                 return rgbaf(rgbB, true);
         }
     }
