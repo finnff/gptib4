0989e7d4a2405a3c2bd177cae534423497f1bcb8
finnf <finnf@protonmail.com>
2019-04-10 14:34:41 +0200

    ss

--- GPTIVB4/BLIKJE/turboscanner2000AMGm3.cpp
+++ GPTIVB4/BLIKJE/turboscanner2000AMGm3.cpp
@@ -39,7 +39,7 @@ vector<vector<long int>> bestand = {
 
 void exit_signal_handler(int signo);
 void kleurscanA();
-void startup();
+//void startup();
 
 // void playsound(){
 //     string tmp1 = to_string(audioID);
