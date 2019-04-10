5e65f2b703d9cb3522e6b0d80a1116840d8ff4ac
HarrySpierenburg <harry.spierenburg@student.hu.nl>
2019-04-10 13:20:28 +0200

    aada

--- GPTIVB4/BLIKJE/audiotest.cpp
+++ GPTIVB4/BLIKJE/audiotest.cpp
@@ -1,7 +1,15 @@
 #include <stdlib.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    system("omxplayer /home/pi/gptib4/GPTIVB4/BLIKJE/audio/Cola.ogg");
+    string tmp1;
+    cout << "kies een bestand" << flush;
+    cin >> tmp1;
+    string call = "omxplayer /home/pi/gptib4/GPTIVB4/BLIKJE/audio/";
+    tmp1 = tmp1 + ".ogg";
+    string newcall = call + tmp1;
+    system(newcall);
 }
